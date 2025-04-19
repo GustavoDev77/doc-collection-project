08842a79e6a07cfc2e23277e57218c8b42d6181e
Gustavodev77 <gustavodev77@gmail.com>
2025-04-19 13:43:13 -0300

    deletar usuarios

--- src/main/java/com/document/project/resources/UserResources.java
+++ src/main/java/com/document/project/resources/UserResources.java
@@ -39,4 +39,10 @@ public class UserResources {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
