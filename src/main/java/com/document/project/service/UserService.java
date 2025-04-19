0c4f6c6b467a766ed96d339077df540cc03940b1
Gustavodev77 <gustavodev77@gmail.com>
2025-04-18 22:17:46 -0300

    Boas praticas para tratar erros

--- src/main/java/com/document/project/service/UserService.java
+++ src/main/java/com/document/project/service/UserService.java
@@ -2,10 +2,12 @@ package com.document.project.service;
 
 import com.document.project.domain.User;
 import com.document.project.repository.UserRepository;
+import com.document.project.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,7 +15,15 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> userOptional = repo.findById(id);
+        if (!userOptional.isPresent()) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return userOptional.get();
     }
 }
---
+++ src/main/java/com/document/project/service/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.document.project.service.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
