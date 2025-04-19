5925252b2d5a5cea24e71353a7f8aa6c03b16577
Gustavodev77 <gustavodev77@gmail.com>
2025-04-19 16:50:17 -0300

    Adição do post dpor autor

--- src/main/java/com/document/project/config/Instantiation.java
+++ src/main/java/com/document/project/config/Instantiation.java
@@ -1,15 +1,23 @@
 package com.document.project.config;
 
+import com.document.project.domain.Post;
 import com.document.project.domain.User;
+import com.document.project.repository.PostRepository;
 import com.document.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -17,12 +25,20 @@ public class Instantiation implements CommandLineRunner {
     @Override
     public void run(String... arg0) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Jacksons five", "jackson@gmail.com");
         User alex = new User(null, "Alex Brown", "brown@gmail.com");
         User gustavo = new User(null, "Gustavo Fring", "fring@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Itaquer", "Ver o Coringão ganhar", gustavo);
+        Post post2 = new Post(null, sdf.parse("04/07/2012"), "Dia de São Nunca", "Vencemos a liberta, vai Corinthians", gustavo);
+
         userRepository.saveAll(Arrays.asList(maria, alex, gustavo));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/document/project/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.document.project.repository;
+
+import com.document.project.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
